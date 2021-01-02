a1e0cbce957d0259a57f903b893e54cf09dd9d62
HocheolJang <daaan0415@gmail.com>
2021-01-02 23:16:20 +0900

    Restore order.vue

--- src/components/common/adminDataTable/AdminFilter.vue
+++ src/components/common/adminDataTable/AdminFilter.vue
@@ -2,9 +2,7 @@
   <div class="filterContainer">
     <div class="filterList">
       <div class="filterTitle">
-        <slot name="filterTitle1">
-          <div<span>필터 div/span> -->
-        </slot>
+        <slot name="filterTitle1"> <span>필터 </span> </slot>
       </div>
       <div class="filterBtnsGroup">
         <select v-model="filterSelectedCondition" class="searchCondition">
@@ -166,23 +164,23 @@ export default {
         { text: "핸드폰번호", value: "senderPhone", disabled: false },
         { text: "--------------------", value: "", disabled: true },
         { text: "셀러명", value: "sellerName", disabled: false },
-        { text: "상품명", value: "productName", disabled: false },
+        { text: "상품명", value: "productName", disabled: false }
       ],
       payedCompletedDateList: [
         {
           name: "payedDate",
           value: "전체",
           id: "payedDateAll",
-          model: "payedCompletedDate",
-        },
-      ],
+          model: "payedCompletedDate"
+        }
+      ]
     };
   },
   methods: {
-    filterSearch: function (event) {
+    filterSearch: function(event) {
       alert("검색 완료!");
     },
-    filterReset: function (event) {
+    filterReset: function(event) {
       (this.filterSelectedCondition = ""),
         (this.searchInputData = ""),
         (this.payedCompletedDate = "3"),
@@ -190,7 +188,7 @@ export default {
         (this.sellerType = "전체"),
         (this.deliveryType = "전체");
     },
-    getToday: function () {
+    getToday: function() {
       let currentDate = new Date().toJSON().slice(0, 10);
       let today = new Date();
       let year = today.getFullYear();
@@ -198,7 +196,7 @@ export default {
       let day = today.getDate();
       this.currentDate = currentDate;
       // alert(`year: ${year}, month: ${month}, day: ${day}`);
-    },
+    }
   },
   computed: {
     getStartedDate: {
@@ -210,14 +208,13 @@ export default {
         let month = parseInt(today.getMonth());
         let day = parseInt(today.getDate());
 
-        return (this.startedDate = `${year}-${month}-${
-          day - payedCompletedDate
-        }`);
-      },
+        return (this.startedDate = `${year}-${month}-${day -
+          payedCompletedDate}`);
+      }
     },
 
     selectAllSellerAttribute: {
-      get: function (value) {
+      get: function(value) {
         if (this.sellerAttribute.length === 0) {
           return true;
         } else if (
@@ -228,22 +225,22 @@ export default {
           return true;
         }
       },
-      set: function (value) {
+      set: function(value) {
         let sellerAttribute = [];
         if (value < this.sellerAttributeList.length) {
-          this.sellerAttributeList.forEach((attribute) => {
+          this.sellerAttributeList.forEach(attribute => {
             sellerAttribute.push(attribute.value);
           });
           this.sellerAttribute = sellerAttribute;
         } else if (value === this.sellerAttributeList.length) {
           this.sellerAttribute = [];
         }
-      },
-    },
+      }
+    }
   },
   mounted() {
     this.getToday();
-  },
+  }
 };
 </script>
 
